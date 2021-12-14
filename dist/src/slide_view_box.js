d8820e3c7500f61d88d550c1cc8c0b6f753d0b01
SA K <sakang07@gmail.com>
2021-12-14 19:39:42 +0900

    [fix] advertise area js

--- dist/src/slide_view_box.js
+++ dist/src/slide_view_box.js
@@ -1,9 +1,14 @@
 // slide_view_box.js
 
 // ==============================================
-// 시나리오... 정리...
-
+// 1. data 불러오기
+// 2. 불러온 data의 길이만큼 슬라이드와 인디케이터 생성
+// 3-1. 슬라이드 좌우 버튼 동작
+// 3-2. 슬라이드 좌우 버튼에 따른 인디케이터 동작
+// 4. 인디케이터 클릭하면 해당 슬라이드로 이동
 // ==============================================
+
+{
 
 // data : 꼭 3개 이상 작성!!
 const BANNER_DATA = [{
@@ -29,6 +34,12 @@ const BANNER_DATA = [{
   contentText : '공차가 선물하는 달콤한 충전<br />초코 신메뉴 마시면 공차 스탬프 3배 적립!',
   url : '#',
   img : 'banner_01.png'
+},
+{
+  titleText : 'We serve you <br />all about choco',
+  contentText : '공차가 선물하는 달콤한 충전<br />초코 신메뉴 마시면 공차 스탬프 3배 적립!',
+  url : '#',
+  img : 'banner_01.png'
 }]
 
 // 이벤트 날짜 기준 순서 뒤집기
@@ -37,7 +48,7 @@ BANNER_DATA.reverse();
 // ==============================================
 
 // 변수-------------------------------------
-// const elViewBox = document.querySelector('#viewBox');
+const elViewBox = document.querySelector('#viewBox');
 
 // pc_view_arrow
 const elViewBtn = elViewBox.querySelector('.view_btn');
@@ -58,6 +69,7 @@ const elViewCont = elViewBox.querySelector('.view_content');
 
 const sildeLen = BANNER_DATA.length;
 
+
 // 기능-------------------------------------
 
 // view_content > ul 생성해서 집어넣기
@@ -65,7 +77,7 @@ const mkViewContUl = document.createElement('ul');
 elViewCont.append(mkViewContUl);
 const elViewContUl = elViewCont.querySelector('ul');
 
-// BANNER_DATA의 길이만큼 li.view_content_inner 요소 생성
+// data의 길이만큼 li.view_content_inner 요소 생성
 for (let i = 0; i < sildeLen; i++) {
   const _mkViewContLi = document.createElement('li');
   let _dataSelect = BANNER_DATA[i];
@@ -83,13 +95,13 @@ for (let i = 0; i < sildeLen; i++) {
   const _elViewImg = elViewCont.querySelectorAll('.view_img');
   _elViewImg[i].style.backgroundImage = `url(${_imgPath}${_dataSelect.img})`;
 }
-// BANNER_DATA의 길이만큼 ul width 설정
+// data의 길이만큼 ul width 설정
 elViewContUl.style.width = `calc(100% * ${sildeLen})`;
 
-// BANNER_DATA의 길이만큼 mobile .view_indicator 숫자 변경
+// data의 길이만큼 mobile .view_indicator 숫자 변경
 elIndiNumTotal.innerText = sildeLen;
 
-// BANNER_DATA의 길이만큼 pc .view_indicator 생성
+// data의 길이만큼 pc .view_indicator 생성
 for (let i = 0; i < sildeLen; i++) {
   let _dataSelect = BANNER_DATA[i];
 
@@ -104,35 +116,28 @@ for (let i = 0; i < sildeLen; i++) {
 const elIndiCircleLi = elIndiCircle.querySelectorAll('li');
 const elIndiCircleLiA = elIndiCircle.querySelectorAll('li > a');
 
+
 // 함수-------------------------------------
 
 const slideW = 100 / sildeLen;
-let i = 0;
+let ckIdx = 0;
 let permission = true;
 
-// 다음 슬라이드로 이동하는 함수
-fnNextSlide = el => {
-  i++;
-  el.style.transform = `translateX(-${i * slideW}%)`;
-  el.style.transition = 'all 500ms ease';
-};
-
-// 이전 슬라이드로 이동하는 함수
-fnPrevSlide = el => {
-  i--;
-  el.style.transform = `translateX(-${i * slideW}%)`;
+// 해당 슬라이드로 이동하는 함수
+fnMoveSlide = (el, idx) => {
+  el.style.transform = `translateX(-${idx * slideW}%)`;
   el.style.transition = 'all 500ms ease';
 };
 
 // 다음 인디케이터로 이동하는 함수
-fnNextIndiCir = el => {
-  el.forEach((_d, _i) => {
-    _i === i ? el[_i].classList.add(ckActive) : el[_i].classList.remove(ckActive);
+fnNextIndiCir = (el, idx) => {
+  el.forEach((d, i) => {
+    i === idx ? el[i].classList.add(ckActive) : el[i].classList.remove(ckActive);
   })
 };
 
 // 전체 배열 중 선택한 순번을 제외한 나머지 형제 선택하는 함수
-const fnSiblings = (select, idx = ckIdx) => {
+const fnSiblings = (select, idx) => {
   const otherArr = [];
   select.forEach((d,i) => {
     if(idx !== i) {
@@ -152,13 +157,14 @@ elViewBtnNext.addEventListener('click', e => {
   if(permission) {
     permission = false;
 
-    if (i < sildeLen - 1) { 
-      fnNextSlide(elViewContUl);
-      fnNextIndiCir(elIndiCircleLi);
+    if (ckIdx < sildeLen - 1) { 
+      ckIdx++;
+      fnMoveSlide(elViewContUl, ckIdx);
+      fnNextIndiCir(elIndiCircleLi, ckIdx);
     } else {
       elViewContUl.style.transform = null;
-      i = 0;
-      fnNextIndiCir(elIndiCircleLi);
+      ckIdx = 0;
+      fnNextIndiCir(elIndiCircleLi, ckIdx);
     }
     permission = true;
   }
@@ -171,14 +177,14 @@ elViewBtnPrev.addEventListener('click', e => {
   if(permission) {
     permission = false;
 
-    if (0 < i && i < sildeLen) { 
-      fnPrevSlide(elViewContUl);
-      fnNextIndiCir(elIndiCircleLi);
+    if (0 < ckIdx && ckIdx <= sildeLen - 1) { 
+      ckIdx--;
+      fnMoveSlide(elViewContUl, ckIdx);
+      fnNextIndiCir(elIndiCircleLi, ckIdx);
     } else {
-      console.log(i);
-      i = sildeLen;
-      fnPrevSlide(elViewContUl);
-      fnNextIndiCir(elIndiCircleLi);
+      ckIdx = sildeLen - 1;
+      fnMoveSlide(elViewContUl, ckIdx);
+      fnNextIndiCir(elIndiCircleLi, ckIdx);
     }
     permission = true;
   }
@@ -186,29 +192,27 @@ elViewBtnPrev.addEventListener('click', e => {
 
 // pc: 인디케이터 누르면 해당 슬라이드 이동
 elIndiCircleLiA.forEach((d, i) => {
-  console.log(d, i);
-
   d.addEventListener('click', e => {
     e.preventDefault();
+    // 클릭한 요소의 순서 파악
+    const ckIdx = i;
     
     // .on 포함한 클래스 파악
     const ckContains = d.parentNode.classList.contains(ckActive);
-    
-    // 클릭한 요소에 .on이 없으면 추가하고 나머지 .on 제거
-    if(!ckContains) {
-      const ckIdx = i
-      d.parentNode.classList.add(ckActive);
 
-      const otherArr = [];
-      d.forEach((d,i) => {
-        if(idx !== i) {
-          otherArr.push(d);
-        }
-      });
-      return otherArr;
-      // fnSiblings(d,i).forEach(el => {
-      //   el.classList.remove(ckActive);
-      // });
-    } 
+    // 전체 요소 중에 누른 요소를 제외한 요소에 .on 제거
+    fnSiblings(elIndiCircleLi).forEach(d => {
+      d.classList.remove(ckActive);
+    })
+
+    // 클릭한 요소에 .on이 없으면 추가
+    if(!ckContains){
+      d.parentNode.classList.add(ckActive);
+    }
+
+    // 클릭한 요소의 순번으로 슬라이드 이동
+    fnMoveSlide(elViewContUl, ckIdx);
   });
 });
+
+}
