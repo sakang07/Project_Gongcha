74c58785dc2c79afe2d6a2f6094c9ac757077be4
SA K <sakang07@gmail.com>
2021-12-16 12:39:35 +0900

    [fix] liveBox pc width DOM error

--- js/src/dom_modify.js
+++ js/src/dom_modify.js
@@ -1,9 +1,12 @@
 // dom_modify.js
 
 // 시나리오 : 
-// 모바일 html 레이아웃 변형
-// 1. .container 삭제
-// 2. .container 안의 .store_search_box와 .social_area를 .container 밖으로 빼기 
+// 1. 모바일 DOM 변경
+// 1-1. .container 삭제
+// 1-2. .container 안의 .store_search_box와 .social_area를 .container 밖으로 빼기 
+
+// 2. 타블렛 DOM 변경
+// 2-1. .live_img_03, .live_img_04의 생성
 
 
 (() => {
@@ -17,6 +20,16 @@
 
   const elLiveCon = elLiveBox.querySelector('.live_content');
 
+  const mkLi3 = document.createElement('li');
+    mkLi3.setAttribute('class', 'live_img_03');
+    mkLi3.innerHTML = '<span class="blind">세 번째 사진</span>';
+  
+  const mkLi4 = document.createElement('li');
+    mkLi4.setAttribute('class', 'live_img_04');
+    mkLi4.innerHTML = '<span class="blind">네 번째 사진</span>';
+
+
+
   // 함수
   // 브라우저가 1024px보다 작거나 작게 리사이징 되면 실행하는 함수
   const fnWidthHandhelds = () => {
@@ -27,32 +40,36 @@
       elContainer.remove();
       elLiveBox.prepend(elStoreSearch)
       elLiveBox.append(elSocialArea);
+    } else {
+      console.log('dfsf');
+      elStoreSearch.remove();
+      elSocialArea.remove();
+      elLiveBox.append(elContainer);
+      elContainer.append(elStoreSearch, elSocialArea);
     }
   }
-  fnWidthHandhelds();
-
     // 브라우저가 600px ~ 1023px 이거나 리사이징 되면 실행하는 함수
-    // const fnWidthHandhelds = () => {
-    //   const browserW = this.matchMedia("all and (min-width: 600px) and (max-width: 1023px)").matches;
-    //   const resizeW = this.innerWidth;
-    //   if(browserW || resizeW > 600) {
-    //     // 세 번째 요소 생성, 삽입
-    //     const mkLi3 = document.createElement('li');
-    //     mkLi3.setAttribute('class', 'live_img_03');
-    //     mkLi3.innerHTML = 'span class="blind">세 번째 사진</span>';
-    //     elLiveCon.append(mkLi3);
+    const fnWidthTablet = () => {
+      const browserW = this.matchMedia("all and (min-width: 600px) and (max-width: 1023px)").matches;
+      const resizeW = this.innerWidth;
 
-    //     // 네 번째 요소 생성, 삽입
-    //     const mkLi4 = document.createElement('li');
-    //     mkLi4.setAttribute('class', 'live_img_04');
-    //     mkLi4.innerHTML = 'span class="blind">네 번째 사진</span>';
-    //     elLiveCon.append(mkLi4);
-    //   }
-    // }
+      if(browserW || resizeW > 600) {
+        elLiveCon.append(mkLi3);
+        elLiveCon.append(mkLi4);
+      } else {
+        mkLi3.remove();
+        mkLi4.remove();
+      }
+    }
+
+    fnWidthHandhelds();
+    fnWidthTablet();
 
   // 이벤트
   // resize 되면 실행
   window.addEventListener('resize', e => {
     fnWidthHandhelds();
+    fnWidthTablet();
+
   })
 })();
