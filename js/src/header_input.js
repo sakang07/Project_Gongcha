d4acbd0373c0fca4fd84eafa17780ba274584dfe
SA K <sakang07@gmail.com>
2021-12-30 18:23:33 +0900

    [refactor] Refactoring main slide

--- js/src/header_input.js
+++ js/src/header_input.js
@@ -1,10 +1,10 @@
 // header_input.js
 
 // 1. 브라우저 너비 파악
-// 2. 너비에 따라 모바일/PC template 삽입(innerText)
-// 3. 모바일/PC 스크립트 삽입. 이미 삽입했으면 기존 것 삭제
+// 2. 너비에 따라 모바일/PC template 삽입(innerText) 후 모바일/PC 스크립트 삽입. 이미 삽입했으면 기존 것 삭제
+// 3. 브라우저 너비가 바뀌면(change) 다시 해당하는 template과 script 불러오기
 
-(() => {
+{
 
   // 변수 ------------------------------------------
   // data url
@@ -32,11 +32,11 @@
   // 기능 ------------------------------------------
   // 기능 함수
 
+  // script 생성 및 삽입하는 함수
   const fnMkScript = data => {
     // .headerScript 요소가 존재한다면 선택 후 삭제(기존 script 문서 삭제)
     const ckScript = document.querySelector('.headerScript');
     if (!!ckScript) {ckScript.remove();}
-
     // 새로운 기능을 가진 script를 생성 및 삽입
     const mkScript = document.createElement('script');
     mkScript.setAttribute('src', data);
@@ -44,9 +44,10 @@
     elBody.append(mkScript);
   };
 
+  // 브라우저 너비를 파악 후 해당 data 삽입하는 함수
   const fnCkMedia = (type = mediaMatches.matches) => {
     let num = type ? 0 : 1; // mobile이면 0, pc면 1
-
+    // data 불러오기
     fetch(setDevice[num].htmlUrl)
       .then(response => response.text())
       .then(data => {elHeadBox.innerHTML = data})
@@ -62,4 +63,4 @@
     fnCkMedia(e.matches);
   });
 
-})();
+}
--- js/src/slide_input.js
+++ js/src/slide_input.js
@@ -4,131 +4,95 @@
 // 2. 브라우저 너비에 따라 모바일일 때는 mob_main_slide.html과 slide_function_mob.js를 불러오기
 // 3. pc일 때는 pc_main_slide.html과 slide_function_pc.js를 불러오기
 // 4. 브라우저 너비가 바뀌면(change) 다시 해당하는 html과 script 불러오기
-// * 비동기식 작동 손보기
-
-let BANNER_DATA;
 
 {
 
   // 변수 =========================================
-  // 식별자
-  const elBody = document.querySelector('body');
-  const elHeadBox = document.querySelector('#headBox');
-  const mediaArray = [];
-
   // data url
-  const data = '../data/slide_data.json';
-  const dataDevice = [
+  const jsonData = '../data/slide_data.json';
+  const setDevice = [
     {
-      type: 'handhelds',
+      type: 'mobile',
       htmlUrl: './temp/mob_main_slide.html',
       scriptUrl: '../js/src/mob_slide_function.js',
       size: 1023,
     }, {
-      type: 'desktop',
+      type: 'pc',
       htmlUrl: './temp/pc_main_slide.html',
       scriptUrl: '../js/src/pc_slide_function.js',
-  }];
+    }
+  ];
+  const mediaSize = `screen and (max-width:${setDevice[0].size}px)`; // mobile 환경
+  const mediaMatches = window.matchMedia(mediaSize); // mobile일 때 .matches 값 true
+
+  // 선택자
+  const elBody = document.querySelector('body');
+  const elWrap = elBody.querySelector('#wrap');
+  const elViewBox = document.querySelector('#viewBox');
 
 
 
   // 기능 =========================================
+  // 기능 함수
 
-  // mediaArray 배열에 device 사이즈별 matchMedia 값을 요소로 할당
-  for (let i = 0; i < dataDevice.length; i++ ) {
-    let _matchCode;
-    if (i === 0) {
-      _matchCode = window.matchMedia(`screen and (max-width:${dataDevice[i].size}px)`);
-    } else if (i === dataDevice.length - 1) {
-      _matchCode = window.matchMedia(`screen and (min-width:${dataDevice[i-1].size + 1}px)`);
-    } else {
-      _matchCode = window.matchMedia(`screen and (min-width:${dataDevice[i-1].size + 1}px) and (max-width:${dataDevice[i].size}px)`);
-    }
-    mediaArray.push(_matchCode);
-  }
+  // script 생성 및 삽입하는 함수
+  const fnMkScript = data => {
+    // .headerScript 요소가 존재한다면 선택 후 삭제(기존 script 문서 삭제)
+    const ckScript = document.querySelector('.slideScript');
+    if (!!ckScript) {ckScript.remove();}
 
-
-  // 함수 =========================================
-
-  // section 요소 생성 및 삽입하는 함수
-  const fnMkEl = value => {
-    const mkEl = document.createElement('section');
-    mkEl.setAttribute('id', value);
-    return mkEl;
-  };
-
-  // fetch로 불러와서 viewBox에 삽입하는 함수
-  const fnFetchTemp = deviceUrl => {
-    const mkViewBox = fnMkEl('viewBox');
-
-    // 이미 #viewBox가 있으면 삭제
-    const _elViewBox = document.querySelector('#viewBox');
-    if(!!_elViewBox) { _elViewBox.remove() }
-
-    fetch(deviceUrl)
-      .then(response => response.text())
-      .then(data => mkViewBox.innerHTML = data)
-      .then(elHeadBox.after(mkViewBox));
-  };
-
-  // script를 Body 내부의 끝에 넣어주는 함수
-  const fnScript = data => {    
-    // 이미 script가 있으면 삭제
-    const _elScript = document.querySelector('.viewBoxScript');
-    if(!!_elScript) { _elScript.remove() }
-
+    // 새로운 기능을 가진 script를 생성 및 삽입
     const mkScript = document.createElement('script');
-    mkScript.src = data;
-    mkScript.setAttribute('class', 'viewBoxScript');
+    mkScript.setAttribute('src', data);
+    mkScript.setAttribute('class', 'slideScript');
     elBody.append(mkScript);
-  }
-
-  // 어떤 디바이스인지에 따라 해당 코드를 수행하는 함수
-  const fnCkDevice = (device, index) => {
-    // 브라우저 너비가 해당 디바이스면
-    if (device.matches) {
-      let _type = dataDevice[index].type; // type에 해당 디바이스 이름 할당
-      // console.log(_type);
-      if (_type === 'handhelds') {
-        // 모바일일 때 수행할 코드
-        fnFetchTemp(dataDevice[0].htmlUrl);
-        fnScript(dataDevice[0].scriptUrl);
-      } else {
-        // pc일 때 수행할 코드
-        fnFetchTemp(dataDevice[1].htmlUrl);
-        fnScript(dataDevice[1].scriptUrl);
-      }
-    }
-  }
-
-  // slide_data.json을 가져오기
-  const fnFetchJson = () => {
-    fetch(data)
-    .then(response => response.json())
-    .then(data => {
-      BANNER_DATA = [...data];
-      
-      // 이벤트 날짜 기준 순서 뒤집기
-      return BANNER_DATA.reverse(); 
-    });
   };
 
-  // mediaArray 배열의 요소들을 순환하며 수행
-  mediaArray.forEach((type, index) => {
-    fnFetchJson(console.log);
-    fnCkDevice(type, index);
-  });
+  // json data를 불러오는 함수
+  const fnFetchJson = data => {
+    return new Promise((resolve) => {
+      fetch(jsonData)
+        .then(response => response.json())
+        .then(data => {
+          BANNER_DATA = [...data];
+          // 이벤트 날짜 기준 순서 뒤집기
+          BANNER_DATA.reverse();})
+          .then( ()=>{
+            resolve();
+          })
+    })
+  }
 
+  // template과 script를 불러오는 함수
+  const fnFetchData = data => {
+    return new Promise((resolve) => {
+      let num = !!data ? 0 : 1; // mobile이면 0, pc면 1
+      fetch(setDevice[num].htmlUrl)
+        .then(response => response.text())
+        .then(data => {elViewBox.innerHTML = data})
+        .then(() => { fnMkScript(setDevice[num].scriptUrl)})
+        .then( ()=>{
+          resolve();
+        });
+    })
+  }
+
+  // 브라우저 너비를 파악 후 해당 data 삽입하는 함수
+  const fnCkMedia = (type = mediaMatches.matches) => {
+    (async () => {
+      await fnFetchJson();
+      fnFetchData(type);
+    })();
+  };
+
+  // 기능 수행
+  fnCkMedia(mediaMatches.matches);
 
   
   // 이벤트 =========================================
 
-  mediaArray.forEach((type, index) => {
-    // 브라우저가 다른 값으로 바뀌었을 때
-    type.addEventListener('change', (e) => {
-      fnCkDevice(type, index);
-    });
+  mediaMatches.addEventListener('change', e => {
+    fnCkMedia(e.matches);
   });
-
 
 }
