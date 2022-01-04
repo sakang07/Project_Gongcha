50f431b1d5e64eec999a2dea40a41f108dad0304
SA K <sakang07@gmail.com>
2022-01-04 18:05:07 +0900

    [fix] Fix button function of pc main menuBox

--- js/src/menu_list_input.js
+++ js/src/menu_list_input.js
@@ -4,7 +4,6 @@
 // 1-2.      오른쪽으로 더 이동할 수 없으면 next 버튼 삭제
 // 1-3.      prev 버튼을 누르면 슬라이드가 왼쪽으로 하나씩 이동, next 버튼 노출
 // 1-4.      왼쪽으로 더 이동할 수 없으면 prev 버튼 삭제
-// 1-5.      시작점이 container에 맞춰 정렬
 // 2-1. mob : 터치로 왼쪽으로 당기면 슬라이드가 오른쪽으로 부드럽게 이동
 // 2-2.       터치로 오른쪽으로 당기면 슬라이드가 왼쪽으로 부드럽게 이동
 {
@@ -12,12 +11,12 @@
   
   // 변수
   const elMenuBox = document.querySelector('#menuBox');
-  const elContainer = elMenuBox.querySelector('.container');
+  // const elContainer = elMenuBox.querySelector('.container');
   const elSlideWrap = elMenuBox.querySelector('.slide_wrap');
   
   const elMenuBtn = elSlideWrap.querySelector('.menu_btn_pc');
-  const elNextBtn = elMenuBtn.querySelector('.next');
-  const elPrevBtn = elMenuBtn.querySelector('.prev');
+  // const elNextBtn = elMenuBtn.querySelector('.next');
+  // const elPrevBtn = elMenuBtn.querySelector('.prev');
   
   const elMenuArea = elSlideWrap.querySelector('.menu_area');
   const elMenuLi = elMenuArea.querySelectorAll('li');
@@ -25,67 +24,81 @@
 
   const slideStyle = elMenuArea.style;
   const slideLen = elMenuLiArr.length; // li 갯수
-  console.log(slideLen);
-  
-  const slideWidth = parseInt(window.getComputedStyle(elMenuLi[0]).width); // li width 추출
-  const slideMaginRight = parseInt(window.getComputedStyle(elMenuLi[0]).marginRight); // li margin_right 추출
-  const slidePx = slideWidth + slideMaginRight;
-  // slideStyle.width = `${slidePx * slideLen - slideMaginRight}px`;
 
-  let slideDefault; // 초기 슬라이드 translateX 값
+  let slideWidth, slideTrs, limitTrs; // 슬라이드 너비, 슬라이드 translateX값, 최대 translateX값
 
-  const TIME = 300;
-  let SLIDE_COUNT = 0;
+  const TIME = 300; // 슬라이드 이동 애니메이션 시간
+  let SLIDE_COUNT = 0; // 슬라이드 순번
+  const CK_ON = 'on'; // 활성화 클래스
 
 
   // 기능
-  // 전체 박스 시작점이 container 박스와 정렬
-  const fnArrange = () => {
-    const containerWidth = parseInt(window.getComputedStyle(elContainer).width); // .container width
-    const menuBoxPadding = parseInt(window.getComputedStyle(elMenuBox).paddingLeft); // #menuBox padding-left
+  // 슬라이드 너비 체크
+  const fnCkSlide = () => {
+    const _liWidth = parseInt(window.getComputedStyle(elMenuLi[0]).width); // li width 추출
+    const _liMaginRight = parseInt(window.getComputedStyle(elMenuLi[0]).marginRight); // li margin_right 추출
+    slideWidth = _liWidth + _liMaginRight; // 슬라이드 1개 너비
+    slideTrs = slideWidth * SLIDE_COUNT; // 슬라이드 이동값
+    const _slideWrapWidth = slideWidth * slideLen; // 슬라이드 전체 너비
+    limitTrs = _slideWrapWidth - window.innerWidth + slideWidth; // 슬라이드가 이동할 수 있는 최대 X값
+    console.log(slideTrs, limitTrs, _slideWrapWidth)
+  }
   
-    slideDefault = (window.innerWidth - containerWidth) / 2 - menuBoxPadding;
-    slideStyle.transform = `translateX(${slideDefault}px)`;
+  // 슬라이드 이동
+  const fnMoveSlide = (e, _btnArr) => {
+    fnCkSlide(); // 슬라이드 너비 체크
+    
+    if (!slideStyle.transition) {slideStyle.transition = `all ${TIME}ms ease`} // 애니메이션 효과
+
+    if (slideTrs < limitTrs - slideWidth && slideTrs > 0) {
+      // X값이 최대값(에서 슬라이드 1개분 뺀 값)보다 작고 0이나 음수가 아닐 때
+      slideStyle.transform = `translateX(${-(slideTrs)}px)`;
+    } else {
+      // X값이 최대값을 넘었거나 음수일 때
+      slideStyle.transform = slideTrs > slideWidth ? `translateX(${-(limitTrs)}px)` : null; // 이전이면 X값 0, 다음이면 최대X값으로 설정
+
+      // 누른 버튼 삭제
+      _btnArr.forEach((d) => {
+        if (e.target === d) d.classList.remove(CK_ON);
+      });
+    }
   };
 
 
-
-  // 기본 수행
-  fnArrange();
-
   // 이벤트
-  // next 버튼 클릭
-  elNextBtn.addEventListener('click', e => {
+  // 버튼 클릭
+  elMenuBtn.addEventListener('click', e => {
     e.preventDefault();
+    // 무슨 버튼을 눌렀는지 파악
+    // next 버튼을 클릭 : 1. prev 버튼을 노출 2. 슬라이드가 끝났으면 next 버튼을 제거
+    // prev 버튼을 클릭 : 1. next 버튼을 노출 2. 슬라이드가 끝났으면 prev 버튼을 제거
+
+    const _btn = e.currentTarget.children;
+    const _btnArr = [..._btn];
+
+    // next일 때 SLIDE_COUNT++, prev일 때 SLIDE_COUNT--
+    e.target === _btnArr[0] ?  SLIDE_COUNT++ : SLIDE_COUNT--;
+    
+    // 누른 버튼에 .on 주기
+    _btnArr.forEach((d) => {
+      if (e.target !== d) {
+        // 누르지 않은 쪽의 버튼에 .on 삽입
+        if (!d.classList.contains(CK_ON)) {d.classList.add(CK_ON)};
+      }
+    });
+    
     // 슬라이드가 끝날 때까지 작동
-    // - translateX의 값이 window.innerWidth만큼 남아 있어야...?
+    // translateX의 값이 window.innerWidth만큼 남아 있어야 한다
     // 전체 너비에서 window.innerWidth를 뺀 값까지만 이동
-    SLIDE_COUNT++;
-    let slideTrs = slideDefault - slidePx * SLIDE_COUNT; // 슬라이드 이동값
-    let slideWrapWidth = slidePx * slideLen + 1; // 슬라이드 전체 너비
-    let limitTrs = slideWrapWidth - window.innerWidth - slideDefault;
-    console.log(slideTrs, limitTrs)
-      slideStyle.transform = `translateX(${slideTrs}px)`;
-        if (!slideStyle.transition) {slideStyle.transition = `all ${TIME}ms ease`}
-    // if (-(slideTrs) < limitTrs) {
-    //   slideStyle.transform = `translateX(${slideTrs}px)`;
-    //   console.log(slideStyle.transform);
-    //   if (!slideStyle.transition) {slideStyle.transition = `all ${TIME}ms ease`}
-    // } else {
-    //   slideStyle.transform = `translateX(${-(limitTrs)}px)`;
-    //   console.log(slideStyle.transform);
-    //   if (!slideStyle.transition) {slideStyle.transition = `all ${TIME}ms ease`}
-    // }
-
+    fnMoveSlide(e, _btnArr);
   });
 
-  // prev 버튼 클릭
-  elPrevBtn.addEventListener('click', e => {
-
-  });
-  
   // 리사이즈
-  window.addEventListener('resize', fnArrange);
+  window.addEventListener('resize', () => {
+    fnMoveSlide();
+  });
 
+  // 모바일 터치
+  
 
 }
